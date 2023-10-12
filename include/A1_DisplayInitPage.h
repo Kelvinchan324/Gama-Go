42bc8dd0c0c8045f3c17084746b6e5e85f9bed02
Kelvinchan324 <125544722+Kelvinchan324@users.noreply.github.com>
2023-10-12 17:16:18 +0800

    Fix

---
+++ include/A1_DisplayInitPage.h
@@ -0,0 +1,16 @@
+//This header file declares the functions in A1_DisplayInitPage.cpp
+//After the functions are declared here, they can be used in any other .cpp files
+
+#ifndef A1_DisplayInitPage_H
+#define A1_DisplayInitPage_H
+  
+
+  #define LoadBarX 16 // Set how far two elements are separated
+  #define LoadBarY 300 // Set Load bar y location
+  #define LoadBarW 14  // Define the width of each load bar element
+  #define LoadBarH 40  // Define the height of each load bar element
+  #define LoadBarR 4   // Define the radius of round edge of load bar element
+
+  void  TFT_init ();
+
+#endif
---
+++ include/A2_DisplayMainPage.h
@@ -0,0 +1,12 @@
+//This header file declares the functions in A1_DisplayInitPage.cpp
+//After the functions are declared here, they can be used in any other .cpp files
+
+#ifndef A2_DisplayMainPage_H
+#define A2_DisplayMainPage_H
+
+  void ShowID ();
+  void CreateBasicData ();
+  void ShowBasicData ();
+  void DisplayGPSdata ();
+
+#endif
