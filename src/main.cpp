0ff24931f8cac603a80f5ab21a9f836e675b51ec
Kelvinchan324 <125544722+Kelvinchan324@users.noreply.github.com>
2024-01-16 18:29:31 +0800

    release 2024-1-16

--- src/main.cpp
+++ src/main.cpp
@@ -84,31 +84,31 @@ void loop()
     PrintGPSstring();
     DisplayGPSdata();
 
-
-    Serial.println (DosePress);
-    Serial.println (DHTPress);
-    Serial.println (BMPPress);
+    plotDHTgraph();
+    // Serial.println (DosePress);
+    // Serial.println (DHTPress);
+    // Serial.println (BMPPress);
     
-    if (DosePress = true)
-    {
-      plotDoseGraph();
-      Serial.println ("case 1");
-    }
-    else if (DHTPress = true)
-    {
-      plotDHTgraph();
-      Serial.println ("case 2");
-    }
-    else if (BMPPress = true)
-    {
-      plotBMPgraph();
-      Serial.println ("case 3");
-    }
-    else
-    {
-      plotDHTgraph();
-      Serial.println ("case 4");
-    }
+    // if (DosePress = true)
+    // {
+    //   plotDoseGraph();
+    //   Serial.println ("case 1");
+    // }
+    // else if (DHTPress = true)
+    // {
+    //   plotDHTgraph();
+    //   Serial.println ("case 2");
+    // }
+    // else if (BMPPress = true)
+    // {
+    //   plotBMPgraph();
+    //   Serial.println ("case 3");
+    // }
+    // else
+    // {
+    //   plotDHTgraph();
+    //   Serial.println ("case 4");
+    // }
     
     OutputDataString();
     BLE_SendDataString();
